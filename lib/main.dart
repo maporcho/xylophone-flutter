0fc0eca8695f55e01ae2eae35d155d7951ac86ed
Marco Porcho <maporcho@gmail.com>
2019-06-17 20:02:22 -0300

    The app is complete

--- lib/main.dart
+++ lib/main.dart
@@ -9,55 +9,33 @@ class XylophoneApp extends StatelessWidget {
     player.play(fileName);
   }
 
+  Widget xylophoneKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound('note$soundNumber.wav');
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  playSound('note1.wav');
-                },
-              ),
-              FlatButton(
-                color: Colors.orange,
-                onPressed: () {
-                  playSound('note2.wav');
-                },
-              ),
-              FlatButton(
-                color: Colors.yellow,
-                onPressed: () {
-                  playSound('note3.wav');
-                },
-              ),
-              FlatButton(
-                color: Colors.green[600],
-                onPressed: () {
-                  playSound('note4.wav');
-                },
-              ),
-              FlatButton(
-                color: Colors.green[900],
-                onPressed: () {
-                  playSound('note5.wav');
-                },
-              ),
-              FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  playSound('note6.wav');
-                },
-              ),
-              FlatButton(
-                color: Colors.indigo,
-                onPressed: () {
-                  playSound('note7.wav');
-                },
-              )
+              xylophoneKey(color: Colors.red, soundNumber: 1),
+              xylophoneKey(color: Colors.orange, soundNumber: 2),
+              xylophoneKey(color: Colors.yellow, soundNumber: 3),
+              xylophoneKey(color: Colors.green[600], soundNumber: 4),
+              xylophoneKey(color: Colors.green[900], soundNumber: 5),
+              xylophoneKey(color: Colors.blue, soundNumber: 6),
+              xylophoneKey(color: Colors.indigo, soundNumber: 7),
             ],
           ),
         ),
